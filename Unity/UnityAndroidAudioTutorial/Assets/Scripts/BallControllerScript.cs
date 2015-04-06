d5de635aecdb938d121936723454fe1d3c737d89
Rich Friedel <rf3studios.apps@gmail.com>
2015-04-06 18:16:01 -0400

    Part 2: Adding Physics To Our GameObject

---
+++ Unity/UnityAndroidAudioTutorial/Assets/Scripts/BallControllerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BallControllerScript : MonoBehaviour
+    {
+        public float BallSpeed;
+        public float MinXVelocity;
+        public float MaxXVelocity;
+        public float MinYVelocity;
+        public float MaxYVelocity;
+
+        private Rigidbody _rigidbody;
+
+        // Use this for initialization
+        void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                _rigidbody.AddForce(new Vector2(Random.Range(MinXVelocity, MaxXVelocity), Random.Range(MinYVelocity, MaxYVelocity)));
+            }
+        }
+
+        void FixedUpdate()
+        {
+
+        }
+    }
+}
